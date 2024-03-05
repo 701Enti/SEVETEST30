7c4f4fd3e34fe7e29b7c0b6dee6e076fa9b7404c
ZengJinlin <zengjinlin8192@yeah.net>
2024-03-06 00:17:52 +0800

    modified:   components/sevetest30_board/include/lsm6ds3trc.h 	modified:   main/board_ctrl.c 	modified:   main/lsm6ds3trc.c

--- main/lsm6ds3trc.c
+++ main/lsm6ds3trc.c
@@ -32,39 +32,85 @@
 // I2C相关配置宏定义在board_def.h下
 #define LSM6DS3TRC_DEVICE_ADD 0x6A
 
-
-/// @brief 映射数据库中的配置到LSM6DS3TRC硬件寄存器
+/// @brief 映射数据库中的配置到LSM6DS3TRC硬件寄存器,即通过数据库写入设定的寄存器
 /// @param reg_database 导入数据库，这是一个以IMU_reg_mapping_t的数组，其中的条目即单个IMU_reg_mapping_t数据的个数没有规定，
 ///                     并且条目的角标与寄存器地址和数据不需要按任何顺序规律进行对应，每个条目只要包含需要写入的寄存器地址和数据，可以参考lsm6ds3trc.h默认配置宏的数据库构建方式
 /// @param map_data_num 需要映射的数据条目数量即要写入的寄存器的个数，从第0个条目按数字大小顺序写入map_num个条目为止,map_num需要小于等于条目总数量
-void lsm6ds3trc_database_map(IMU_reg_mapping_t* reg_database,int map_num){
-  const char* TAG = "lsm6ds3trc_database_map";
-  if(!reg_database){
-   ESP_LOGE(TAG,"导入了为空的数据库");
-   return;
-  }  
-  //用于偏移到指定数据库内存区域获取数据
-  IMU_reg_mapping_t* mapping_buf = reg_database;
+void lsm6ds3trc_database_map_set(IMU_reg_mapping_t *reg_database, int map_num)
+{
+  const char *TAG = "lsm6ds3trc_database_map_set";
+  if (!reg_database)
+  {
+    ESP_LOGE(TAG, "导入了为空的数据库");
+    return;
+  }
+  // 用于偏移到指定数据库内存区域获取数据
+  IMU_reg_mapping_t *mapping_buf = reg_database;
 
-  for(int idx=0;idx<map_num;idx++){
+  for (int idx = 0; idx < map_num; idx++)
+  {
     mapping_buf = reg_database + sizeof(IMU_reg_mapping_t) * idx;
-    uint8_t write_buf[2] = {mapping_buf->reg_address,mapping_buf->reg_value};
-    esp_err_t err = i2c_master_write_to_device(DEVICE_I2C_PORT,LSM6DS3TRC_DEVICE_ADD,write_buf, sizeof(write_buf), 1000 / portTICK_PERIOD_MS);
-    if (err != ESP_OK){
+    uint8_t write_buf[2] = {mapping_buf->reg_address, mapping_buf->reg_value};
+    esp_err_t err = i2c_master_write_to_device(DEVICE_I2C_PORT, LSM6DS3TRC_DEVICE_ADD, write_buf, sizeof(write_buf), 1000 / portTICK_PERIOD_MS);
+    if (err != ESP_OK)
+    {
       ESP_LOGE(TAG, "与姿态传感器LSM6DS3TRC通讯时发现问题 描述： %s", esp_err_to_name(err));
       return;
-    }  
+    }
   }
-  ESP_LOGI(TAG,"成功映射配置到姿态传感器LSM6DS3TRC");
+  ESP_LOGI(TAG, "成功映射配置到姿态传感器LSM6DS3TRC");
 }
 
+/// @brief 映射LSM6DS3TRC硬件寄存器的数据到数据库,即读取设定的寄存器到数据库
+/// @param reg_database 导入数据库，这是一个以IMU_reg_mapping_t的数组，其中的条目即单个IMU_reg_mapping_t数据的个数没有规定，
+///                     并且条目的角标与寄存器地址和数据不需要按任何顺序规律进行对应，每个条目只要包含需要写入的寄存器地址和数据，可以参考lsm6ds3trc.h默认配置宏的数据库构建方式
+//                      完成读取后,数据库中寄存器的值将更新,因此在初始化数据库用MAP_BASE设定寄存器时的值任意如0x00
+/// @param map_data_num 需要映射的数据条目数量即要写入的寄存器的个数，从第0个条目按数字大小顺序写入map_num个条目为止,map_num需要小于等于条目总数量
+void lsm6ds3trc_database_map_read(IMU_reg_mapping_t *reg_database, int map_num)
+{
+  const char *TAG = "lsm6ds3trc_database_map_read";
+  if (!reg_database)
+  {
+    ESP_LOGE(TAG, "导入了为空的数据库");
+    return;
+  }
+  // 用于偏移到指定数据库内存区域写入数据库
+  IMU_reg_mapping_t *mapping_buf = reg_database;
+
+  for (int idx = 0; idx < map_num; idx++)
+  {
+    mapping_buf = reg_database + sizeof(IMU_reg_mapping_t) * idx;
+
+    esp_err_t err = i2c_master_write_read_device(DEVICE_I2C_PORT, LSM6DS3TRC_DEVICE_ADD, &(mapping_buf->reg_address), sizeof(uint8_t), &(mapping_buf->reg_value), sizeof(uint8_t), 1000 / portTICK_PERIOD_MS);
+
+    if (err != ESP_OK)
+    {
+      ESP_LOGE(TAG, "与姿态传感器LSM6DS3TRC通讯时发现问题 描述： %s", esp_err_to_name(err));
+      return;
+    }
+  }
+}
 
 /// @brief 以默认配置初始化LSM6DS3TRC姿态传感器，或者重置其到默认配置
-void lsm6ds3trc_init_or_reset(){
-  const char* TAG = "lsm6ds3trc_init";
-  IMU_reg_mapping_t reg_database[IMU_REG_NUMBER] = IMU_INIT_DEFAULT_MAPPING_DATABASE;//寄存器映射数据库 
-  lsm6ds3trc_database_map(reg_database,IMU_REG_NUMBER);
+void lsm6ds3trc_init_or_reset()
+{
+  IMU_reg_mapping_t reg_database[IMU_INIT_DEFAULT_MAPPING_DATABASE_MAP_NUM] = IMU_INIT_DEFAULT_MAPPING_DATABASE; // 寄存器映射数据库
+  lsm6ds3trc_database_map_set(reg_database, IMU_INIT_DEFAULT_MAPPING_DATABASE_MAP_NUM);
 }
 
+/// @brief 获取步数计数值
+/// @return 步数计数值
+uint16_t lsm6ds3trc_data_get_step_counter()
+{
+  IMU_reg_mapping_t reg_database[2] = {
+      MAP_BASE(0, REG_ADD_OUT_TEMP_L, 0x00),
+      MAP_BASE(1, REG_ADD_OUT_TEMP_H, 0x00),
+  };
+  lsm6ds3trc_database_map_read(reg_database, 2);
 
+  uint16_t ret = 0x0000;
+  ret = reg_database[0].reg_value | (reg_database[1].reg_value << sizeof(uint8_t));
+  ESP_LOGE("M", "%d", ret);
 
+  return ret;
+}
